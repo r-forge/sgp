b69d99ff50a19863756e0cb67147ad25abe32896
jds (John Stewart) <johnstewart@aya.yale.edu>
2011-03-05 01:33:15 +0000

    unload generic functions to leave a clean workspace after running unit tests
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/sgp@102 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/SGP/tests/testSGP.R
+++ pkg/SGP/tests/testSGP.R
@@ -1,12 +1,20 @@
+testEnv <- new.env()
+setPackageName('SGP', testEnv)
+load(file.path(getwd(),'pkg/SGP/data/sgpData.rda'), envir=testEnv)
+
 .setUp <- function() {
-	load(file.path(getwd(),'pkg/SGP/data/sgpData.rda'), envir=topenv())
+  sys.source(file.path(getwd(), 'pkg/SGP/R/splineMatrix.R'), envir=testEnv)
+  attach(testEnv)
 }
 
 .tearDown <- function() {
-	rm(sgpData, envir=topenv())
+  removeGeneric(getGenerics(testEnv), where=testEnv)
+  detach(testEnv)
 }
 
 test.test <- function() {
 	checkEquals(sum(sgpData, na.rm=T), 439533703192)
 }
 
+test.splineMatrix.create.knots.boundaries <- function() {
+}
