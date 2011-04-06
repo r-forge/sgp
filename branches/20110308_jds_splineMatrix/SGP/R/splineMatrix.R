af3b8088c6a2a97f69c734577db37545ca46747b
jds (John Stewart) <johnstewart@aya.yale.edu>
2011-04-06 02:20:46 +0000

    function to compute knots and boundaries
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/sgp@121 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- branches/20110308_jds_splineMatrix/SGP/R/splineMatrix.R
+++ branches/20110308_jds_splineMatrix/SGP/R/splineMatrix.R
@@ -1,28 +1,109 @@
+##########################################################
+###
+### Class Definition and Constructor
+###
+##########################################################
+
 setClass(
 	'splineMatrix',
 	representation = representation(
-		data="data.frame", 
-		by.grade="logical"),
+    knots = "numeric",
+    boundaries = "numeric",
+    loss.hoss = "numeric",
+    qrmatrix = "matrix"),
 	prototype = NULL
 )
 
 setMethod(
 	'initialize',
 	'splineMatrix',
-	function(.Object, data, by.grade) {
-		.Object@data = data
-		.Object
+	function(.Object) {
+    .Object@knots = numeric(0)
+    .Object@boundaries = numeric(0)
+    .Object@loss.hoss = numeric(0)
+    .Object@qrmatrix = matrix()
+    
+    .Object
 	}
 )
 
-if (!isGeneric('get.data')) {
-	setGeneric('get.data', function(.Object) standardGeneric('get.data'))
+##########################################################
+###
+### Accessor Functions
+###
+##########################################################
+
+if (!isGeneric('get.knots')) {
+	setGeneric('get.knots', 
+    function(.Object) standardGeneric('get.knots'))
 }
 
 setMethod(
-	'get.data',
+	'get.knots',
 	'splineMatrix',
 	function(.Object) {
-		.Object@data
+		.Object@knots
+	}
+)
+
+if (!isGeneric('get.boundaries')) {
+  setGeneric('get.boundaries', 
+    function(.Object) standardGeneric('get.boundaries'))
+}
+
+setMethod(
+	'get.boundaries',
+	'splineMatrix',
+	function(.Object) {
+		.Object@boundaries
+	}
+)
+
+if (!isGeneric('get.loss.hoss')) {
+  setGeneric('get.loss.hoss', 
+    function(.Object) standardGeneric('get.loss.hoss'))
+}
+
+setMethod(
+	'get.loss.hoss',
+	'splineMatrix',
+	function(.Object) {
+		.Object@loss.hoss
+	}
+)
+
+if (!isGeneric('get.qrmatrix')) {
+  setGeneric('get.qrmatrix', 
+    function(.Object) standardGeneric('get.qrmatrix'))
+}
+
+setMethod(
+  'get.qrmatrix',
+	'splineMatrix',
+	function(.Object) {
+		.Object@qrmatrix
+	}
+)
+
+##########################################################
+###
+### Methods
+###
+##########################################################
+
+if (!isGeneric('create.knots.boundaries')) {
+  setGeneric('create.knots.boundaries', 
+    function(.Object, data, knot.cut.percentiles=c(0.2,0.4,0.6,0.8)) standardGeneric('create.knots.boundaries'))
+}
+
+setMethod(
+  'create.knots.boundaries',
+	'splineMatrix',
+	function(.Object, data, knot.cut.percentiles=c(0.2,0.4,0.6,0.8)) {
+		.Object@knots = round(as.vector(quantile(data, probs=knot.cut.percentiles, na.rm=TRUE)), digits=3)
+    .Object@boundaries <- round(as.vector(extendrange(data, f=0.1)), digits=3)  
+    .Object@loss.hoss <- round(as.vector(extendrange(data, f=0.0)), digits=3)  
+    
+    .Object
 	}
 )
--- branches/20110308_jds_splineMatrix/SGP/tests/testSGP.R
+++ branches/20110308_jds_splineMatrix/SGP/tests/testSGP.R
@@ -16,5 +16,29 @@ test.test <- function() {
 	checkEquals(sum(sgpData, na.rm=T), 439533703192)
 }
 
+test.splineMatrix.constructor <- function() {
+  sm = new("splineMatrix")
+  checkEquals(get.knots(sm), numeric(0))
+  checkEquals(get.boundaries(sm), numeric(0))
+  checkEquals(get.loss.hoss(sm), numeric(0))
+  checkEquals(get.qrmatrix(sm), matrix())
+}
+
 test.splineMatrix.create.knots.boundaries <- function() {
+  sm = new("splineMatrix")
+  
+  tmp.stack = data.table(grades=Reduce(append, sgpData[2:6]), scores=Reduce(append, sgpData[7:11]))
+  data = tmp.stack[tmp.stack$grade == 4,]$scores
+  
+  sm = create.knots.boundaries(sm, data)
+  checkEquals(get.knots(sm), c(544, 581, 608, 635))
+  checkEquals(get.boundaries(sm), c(117.4, 868.6))
+  checkEquals(get.loss.hoss(sm), c(180, 806))
+}
+
+test.splineMatrix.create.coefficient.matrices <- function() {
+  sm = new("splineMatrix")
+
+  tmp.stack = data.table(grades=Reduce(append, sgpData[2:6]), scores=Reduce(append, sgpData[7:11]))
+  data = tmp.stack[tmp.stack$grade == 4,]$scores
 }
